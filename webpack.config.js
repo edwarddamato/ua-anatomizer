c2895f9c734a599966547905605076edd8f5d91f
Edward D'Amato <info@edwarddamato.com>
2017-07-16 23:20:52 +0100

    Generate build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'index_bundle.js'
   },
   module: {
