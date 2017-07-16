3fb16d489c55a4c7e427e16c39f62253cc485bb5
Edward D'Amato <info@edwarddamato.com>
2017-07-16 23:11:48 +0100

    Add basic parser

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { parse } from '../parser';
 import './Form.scss';
 
-const Form = props => (
-  <section className="form">
-    <section className="form_description">
-      <span className="copy copy--extra">
-        <p><strong>anatomize</strong> <em>verb</em></p>
-        <p>To dissect (a body). To examine and analyse in detail.</p>
-      </span>
-      <p>Paste a list of <strong>user agent</strong> strings separated by a new line to generate statistics by browser (and version), operating system and device.</p>
-    </section>
-    <textarea className="form_input" placeholder={props.currentUserAgent} />
-    <button className="form_button">Anatomize!</button>
-  </section>
-);
+class Form extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      currentInput: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleInputChange (e) {
+    this.setState({ currentInput: e.target.value });
+  }
+
+  handleButtonClick () {
+    const inputArr = this.state.currentInput.split('\n');
+    const stats = parse(inputArr);
+    console.table(stats.browsers);
+    console.table(stats.os);
+    console.table(stats.devices);
+  }
+
+  render () {
+    return (
+      <section className="form">
+        <section className="form_description">
+          <span className="copy copy--extra">
+            <p><strong>anatomize</strong> <em>verb</em></p>
+            <p>To dissect (a body). To examine and analyse in detail.</p>
+          </span>
+          <p>Paste a list of <strong>user agent</strong> strings separated by a new line to generate statistics by browser (and version), operating system and device.</p>
+        </section>
+        <textarea className="form_input" onChange={this.handleInputChange} placeholder={this.props.currentUserAgent} />
+        <button className="form_button" onClick={this.handleButtonClick}>Anatomize!</button>
+      </section>
+    );
+  }
+};
 
 Form.propTypes = {
   currentUserAgent: PropTypes.string.isRequired
--- src/components/parser/index.js
+++ src/components/parser/index.js
@@ -1,39 +1,5 @@
 const parser = require('ua-parser-js');
 
-const agents = [
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:54.0) Gecko/20100101 Firefox/54.0',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89',
-  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.8 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5',
-  'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
-  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1',
-  'Mozilla/5.0 (Linux; Android 7.0; SM-G928V Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36',
-  'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)',
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89',
-  'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36',
-  'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36',
-  'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/602.1',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36',
-  'Mozilla/5.0 (iPad; CPU OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F91 Safari/602.1',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
-  'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/603.2.5 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.5',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.6 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.6',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
-];
-
 const stats = {
   browsers: { },
   devices: { },
@@ -51,27 +17,29 @@ const setKeyInStat = (area, key) => {
   }
 };
 
-agents.forEach(agentString => {
-  stats.total++;
-  const agent = parser(agentString);
+const parse = uas => {
+  uas.forEach(agentString => {
+    stats.total++;
+    const agent = parser(agentString);
 
-  const agentBrowser = `${agent.browser.name} ${agent.browser.major}`;
-  setKeyInStat('browsers', agentBrowser);
+    const agentBrowser = `${agent.browser.name} ${agent.browser.major}`;
+    setKeyInStat('browsers', agentBrowser);
 
-  const agentDevice = typeof agent.device.vendor === typeof undefined ? 'Unknown' : `${agent.device.vendor} ${agent.device.model}`;
-  setKeyInStat('devices', agentDevice);
+    const agentDevice = typeof agent.device.vendor === typeof undefined ? 'Unknown' : `${agent.device.vendor} ${agent.device.model}`;
+    setKeyInStat('devices', agentDevice);
 
-  const agentOS = `${agent.os.name} ${agent.os.version.split('.')[0]}`;
-  setKeyInStat('os', agentOS);
-});
-
-Object.keys(stats).forEach(stat => {
-  Object.keys(stats[stat]).forEach(itemKey => {
-    const item = stats[stat][itemKey];
-    item.percentage = `${parseInt((item.count * 100) / stats.total)}%`;
+    const agentOS = `${agent.os.name} ${agent.os.version.split('.')[0]}`;
+    setKeyInStat('os', agentOS);
   });
-});
 
-console.log(stats);
+  Object.keys(stats).forEach(stat => {
+    Object.keys(stats[stat]).forEach(itemKey => {
+      const item = stats[stat][itemKey];
+      item.percentage = `${parseInt((item.count * 100) / stats.total)}%`;
+    });
+  });
 
-// console.log(browser);
+  return stats;
+};
+
+export { parse };
